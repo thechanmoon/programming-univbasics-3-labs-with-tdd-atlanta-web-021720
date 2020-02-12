ac2d395ae95cdcd107932b7076d88e3ad0f36a2e
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-12 19:25:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,25 @@
 # Add your variables here
+first_number = 1
+second_number = 2
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+
+# def sum(first_number, second_number)
+#   first_number + second_number  
+# end  
+
+# def sub(first_number, second_number)
+#   first_number - second_number  
+# end  
+
+# def mul(first_number, second_number)
+#   first_number * second_number  
+# end  
+
+def div(first_number, second_number)
+  first_number / second_number  
+end  
